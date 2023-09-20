85f90eac319ad831e49706ecfb2cf6cbb9baf4dd
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-09-20 14:20:26 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,78 +1,54 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-
-ENABLE_VIRTUAL_AB := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-
-# A/B
-AB_OTA_UPDATER := true
-AB_OTA_PARTITIONS += \
-    boot \
-    dpm \
-    dtbo \
-    gz \
-    lk \
-    logo \
-    mcupm \
-    md1img \
-    pi_img \
-    preloader \
-    product \
-    scp \
-    spmfw \
-    sspm \
-    system \
-    tee \
-    vbmeta \
-    vbmeta_system \
-    vbmeta_vendor \
-    vendor
-
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-# Dynam
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.build.security_patch=2099-12-31
-
-# Health Hal
-PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-service
-
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-mtkimpl.recovery
-
-PRODUCT_PACKAGES_DEBUG += \
-    bootctl
-
-# Fastbootd
-PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock \
-    fastbootd
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
+# 
+ # Copyright (C) 2023 The Android Open Source Project 
+ # 
+ # SPDX-License-Identifier: Apache-2.0 
+ # 
+  
+ # A/B 
+ # Boot control HAL 
+ PRODUCT_PACKAGES += \ 
+     android.hardware.boot@1.2-impl \ 
+     android.hardware.boot@1.2-impl.recovery \ 
+     android.hardware.boot@1.2-service 
+  
+ PRODUCT_PACKAGES += \ 
+     update_engine \ 
+     update_engine_sideload \ 
+     update_verifier 
+  
+ PRODUCT_PACKAGES_DEBUG += \ 
+     update_engine_client 
+  
+ AB_OTA_POSTINSTALL_CONFIG += \ 
+     RUN_POSTINSTALL_system=true \ 
+     POSTINSTALL_PATH_system=system/bin/otapreopt_script \ 
+     FILESYSTEM_TYPE_system=ext4 \ 
+     POSTINSTALL_OPTIONAL_system=true 
+  
+ AB_OTA_POSTINSTALL_CONFIG += \ 
+     RUN_POSTINSTALL_vendor=true \ 
+     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \ 
+     FILESYSTEM_TYPE_vendor=erofs \ 
+     POSTINSTALL_OPTIONAL_vendor=true 
+  
+ PRODUCT_PACKAGES += \ 
+     checkpoint_gc \ 
+     otapreopt_script 
+  
+ PRODUCT_PACKAGES += \ 
+     android.hardware.fastboot@1.0-impl-mock \ 
+     fastbootd 
+  
+ # Keymaster 
+ TARGET_RECOVERY_DEVICE_MODULES += \ 
+     libkeymaster4 \ 
+     libkeymaster41 \ 
+     libpuresoftkeymasterdevice 
+  
+ RECOVERY_LIBRARY_SOURCE_FILES += \ 
+     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \ 
+     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \ 
+     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 
 # OEM otacerts
 PRODUCT_EXTRA_RECOVERY_KEYS += \
