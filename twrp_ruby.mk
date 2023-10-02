c810223b78cccfd8abf3a3ec8e112be20c2366c4
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-10-02 18:38:02 +0800

    Update twrp_ruby.mk

--- twrp_ruby.mk
+++ twrp_ruby.mk
@@ -16,9 +16,33 @@ $(call inherit-product, vendor/twrp/config/common.mk)
 $(call inherit-product, device/xiaomi/ruby/device.mk)
 
 # Device identifier. This must come after all inclusions
+
+
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Inherit from star device
+$(call inherit-product, device/xiaomi/ruby/device.mk)
+
+# Inherit some common recovery stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ruby
 PRODUCT_NAME := twrp_ruby
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 22101316G
-PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_RELEASE_NAME := Redmi Note 12 Pro 5G
+PRODUCT_MANUFACTURER := xiaomi
+
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
