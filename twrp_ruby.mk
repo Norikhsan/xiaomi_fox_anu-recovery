d83a3c28ab684903ae1c32e4fb86c1fc6ea32e5a
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-10-02 18:47:19 +0800

    Update twrp_ruby.mk

--- twrp_ruby.mk
+++ twrp_ruby.mk
@@ -1,25 +1,18 @@
-#
-# Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
-
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
-# Virtual A/B OTA
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
-
-# Inherit from star device
-$(call inherit-product, device/xiaomi/ruby/device.mk)
-
-# Inherit some common recovery stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+# 
+ # Copyright (C) 2023 The Android Open Source Project 
+ # 
+ # SPDX-License-Identifier: Apache-2.0 
+ # 
+  
+ # Inherit from those products 
+ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk) 
+ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk) 
+  
+ # Inherit from device configuration 
+ $(call inherit-product, device/xiaomi/tapas/device.mk) 
+  
+ # Inherit from TWRP common configuration 
+ $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ruby
@@ -28,6 +21,9 @@ PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 22101316G
 PRODUCT_RELEASE_NAME := Redmi Note 12 Pro 5G
 PRODUCT_MANUFACTURER := xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
 
 # HACK: Set vendor patch level
 PRODUCT_PROPERTY_OVERRIDES += \
