61b48f695fb0154dfec785efeb812654f1567c18
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-10-18 04:48:06 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -7,7 +7,25 @@
  # Dynamic Partitions 
  PRODUCT_USE_DYNAMIC_PARTITIONS := true 
 
+ # A/B
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS += \
+    boot \
+    vendor \ 
+    product \ 
+    system \
+    syatem_ext \ 
+    vbmeta_system \
+    vbmeta_vendor 
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
  PRODUCT_PACKAGES += \
+    otapreopt_script \
     update_engine \
     update_engine_sideload \
     update_verifier
