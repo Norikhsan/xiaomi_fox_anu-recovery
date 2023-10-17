45b20fd87e2614df52d13bea7ec969ad686526b0
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-10-18 04:40:23 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -14,15 +14,6 @@
 
  PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
-
- AB_OTA_POSTINSTALL_CONFIG += \ 
-     RUN_POSTINSTALL_system=true \ 
-     POSTINSTALL_PATH_system=system/bin/otapreopt_script \ 
-     FILESYSTEM_TYPE_system=ext4 \ 
-     POSTINSTALL_OPTIONAL_system=true 
-
- PRODUCT_PACKAGES += \ 
-     otapreopt_script 
   
  PRODUCT_PACKAGES += \ 
      android.hardware.fastboot@1.0-impl-mock \ 
