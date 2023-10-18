8989592f2638846a80f504aa565443244aebcd35
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-10-18 21:04:51 +0800

    Update twrp_ruby.mk

--- twrp_ruby.mk
+++ twrp_ruby.mk
@@ -1,4 +1,4 @@
-# 
+ # 
  # Copyright (C) 2023 The Android Open Source Project 
  # 
  # SPDX-License-Identifier: Apache-2.0 
@@ -7,17 +7,23 @@
  # Release name 
  PRODUCT_RELEASE_NAME := ruby
   
- # To reference the default compilation configuration file, core_64_bit must precede other references, otherwise it will not take effect.
- $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk) 
- $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk) 
+ # Inherit from those products. Most specific first. 
+  $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk) 
+  $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk) 
   
- # Inherit from bomb device 
- $(call inherit-product, device/xiaomi/ruby/device.mk) 
+ # Inherit some common TWRP stuff 
+  $(call inherit-product, vendor/twrp/config/common.mk) 
   
- # Device identifier. This must come after all inclusions 
- PRODUCT_DEVICE := ruby
- PRODUCT_NAME := twrp_ruby
- PRODUCT_BRAND := Redmi
- PRODUCT_MODEL := 22101316G
- PRODUCT_RELEASE_NAME := Redmi Note 12 Pro 5G
- PRODUCT_MANUFACTURER := xiaomi
+ # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot. 
+  $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk) 
+  
+ # Inherit from ruby device 
+  $(call inherit-product, device/xiaomi/ruby/device.mk) 
+  
+ # Device identifier. This must come after all inclusions  
+  PRODUCT_DEVICE := ruby 
+  PRODUCT_NAME := twrp_ruby 
+  PRODUCT_BRAND := Redmi 
+  PRODUCT_MODEL := 22101316G 
+  PRODUCT_MANUFACTURER := xiaomi 
+  
