14f714bbfe7a043afc325dd2f9cac3c19ee59c8b
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-10-18 21:31:10 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -12,6 +12,10 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
+  
+ PRODUCT_PACKAGES += \ 
+     bootctrl.mt6877 \ 
+     bootctrl.mt6877.recovery 
 
  PRODUCT_PACKAGES += \
     otapreopt_script \
@@ -21,17 +25,30 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
  PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
+
+  # Boot control HAL 
+ PRODUCT_PACKAGES += \ 
+     android.hardware.boot@1.1-impl \ 
+     android.hardware.boot@1.1-impl.recovery \ 
+     android.hardware.boot@1.2-mtkimpl.recovery 
+    
+# Additional Libraries 
+ TARGET_RECOVERY_DEVICE_MODULES += \ 
+     libkeymaster4 \ 
+     libkeymaster41 \ 
+     libpuresoftkeymasterdevice 
   
- # Fastbootd
-    TW_INCLUDE_FASTBOOTD := true
+ RECOVERY_LIBRARY_SOURCE_FILES += \ 
+     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \ 
+     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \ 
+     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 
-# Additional Libraries
- TARGET_RECOVERY_DEVICE_MODULES += \
-    libkeymaster4 \
-    libkeymaster41 \
-    libpuresoftkeymasterdevice
-
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+ # API 
+ PRODUCT_SHIPPING_API_LEVEL := 32 
+  
+ # VNDK 
+ PRODUCT_TARGET_VNDK_VERSION := 33
+  
+  
+  
+  
