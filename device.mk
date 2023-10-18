7a124a10770d2099657ad80e4f255d5f5c7de66b
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-10-18 23:02:19 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -27,17 +27,5 @@ AB_OTA_POSTINSTALL_CONFIG += \
 
  PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
-
-# Additional Libraries 
- TARGET_RECOVERY_DEVICE_MODULES += \ 
-     libkeymaster4 \ 
-     libkeymaster41 \ 
-     libpuresoftkeymasterdevice 
-  
- RECOVERY_LIBRARY_SOURCE_FILES += \ 
-     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \ 
-     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \ 
-     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
-  
   
   
