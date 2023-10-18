343ead89ef53eca013aebce5122076e540d99787
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-10-18 23:27:04 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,31 +1,25 @@
-# 
+ # 
  # Copyright (C) 2023 The Android Open Source Project 
  # 
  # SPDX-License-Identifier: Apache-2.0 
  # 
 
- # Dynamic Partitions 
- PRODUCT_USE_DYNAMIC_PARTITIONS := true 
+LOCAL_PATH := device/xiaomi/bomb
 
- # API 
- PRODUCT_SHIPPING_API_LEVEL := 32 
-  
- # VNDK 
- PRODUCT_TARGET_VNDK_VERSION := 33
-  
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
- PRODUCT_PACKAGES += \
-    otapreopt_script \
-    update_engine \
-    update_engine_sideload \
-    update_verifier
+# API
+PRODUCT_TARGET_VNDK_VERSION := 33
+PRODUCT_SHIPPING_API_LEVEL := 32
 
- PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
-  
-  
+# Additional binaries & libraries needed for recovery
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster41 \
+    libkeymaster4 \
+    libpuresoftkeymasterdevice
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so 
