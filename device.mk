3846bbaaff628cc0a10d089bc4de1e36d209a0e1
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-10-18 12:15:29 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -22,17 +22,16 @@ AB_OTA_POSTINSTALL_CONFIG += \
  PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
   
- PRODUCT_PACKAGES += \ 
-     android.hardware.fastboot@1.0-impl-mock \ 
-     fastbootd 
+ # Fastbootd
+    TW_INCLUDE_FASTBOOTD := true
 
 # Additional Libraries
-TARGET_RECOVERY_DEVICE_MODULES += \
-     libkeymaster4 \
-     libkeymaster41 \
-     libpuresoftkeymasterdevice
+ TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libkeymaster41 \
+    libpuresoftkeymasterdevice
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
-     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
-     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster41.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
