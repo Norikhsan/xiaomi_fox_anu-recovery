59c0b3f9dc13d484d8978e24b3089524403b2793
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-10-19 18:35:51 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,6 @@ export TARGET_ARCH="arm64-v8a"
 # Some about us
 export FOX_VERSION="R12.1"
 export OF_MAINTAINER=norikhsan90
-export TARGET_DEVICE_ALT="ruby"
 export FOX_TARGET_DEVICES="ruby,rubypro"
 
 # Use magisk boot for patching
@@ -13,7 +12,6 @@ export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
 
 # We have a/b partitions
 export FOX_AB_DEVICE=1
-export FOX_VIRTUAL_AB_DEVICE=1
 
 # Disable Flashlight & Green LED
 export OF_FLASHLIGHT_ENABLE=0
@@ -34,11 +32,11 @@ export OF_FBE_METADATA_MOUNT_IGNORE=1
 export OF_PATCH_AVB20=0
 
 # Run a process after formatting data to work-around MTP issues
-#export OF_RUN_POST_FORMAT_PROCESS=1
+export OF_RUN_POST_FORMAT_PROCESS=1
 
 # Removes the loop block errors after flashing ZIPs (Workaround) 
 export OF_LOOP_DEVICE_ERRORS_TO_LOG=1 
-#export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1 
+export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1 
 
 # MIUI & Custom ROMs 
 export FOX_VIRTUAL_AB_DEVICE=1
@@ -50,19 +48,10 @@ export OF_NO_RELOAD_AFTER_DECRYPTION=1
 export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
 export OF_FBE_METADATA_MOUNT_IGNORE=1 
 export OF_NO_SPLASH_CHANGE=1
-export OF_FIX_DECRYPTION_ON_DATA_MEDIA=1
-
-# For some reason this is dumb and necessary
-#export FOX_BUGGED_AOSP_ARB_WORKAROUND="1601559499"
-
-# Quick Backup list
-#export OF_QUICK_BACKUP_LIST="/boot;/data;/super;"
+#export OF_FIX_DECRYPTION_ON_DATA_MEDIA=1
   
 # Security (Disables MTP&ADB during password prompt) 
 export OF_ADVANCED_SECURITY=1 
-
-# Magisk 26.3
-#export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v26.3.zip
 
 # Dont install AROMAFM
 export FOX_DELETE_AROMAFM=1
@@ -70,11 +59,10 @@ export FOX_DELETE_AROMAFM=1
 # Add some extras
 export FOX_USE_ZIP_BINARY=1
 export FOX_USE_TAR_BINARY=1
-export FOX_ASH_IS_BASH=1
+#export FOX_ASH_IS_BASH=1
 export FOX_REPLACE_BUSYBOX_PS=1
 export FOX_USE_BASH_SHELL=1
 export OF_USE_LZMA_COMPRESSION=1
 export FOX_USE_NANO_EDITOR=1
 export OF_DONT_KEEP_LOG_HISTORY=1
-export OF_NO_SPLASH_CHANGE=1
 export FOX_INSTALLER_DISABLE_AUTOREBOOT=1
