bfb3d692528443749cd66441bc1f4e43d0dea624
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-11-05 21:29:37 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -23,6 +23,35 @@ AB_OTA_POSTINSTALL_CONFIG += \
 PRODUCT_PACKAGES += \
     otapreopt_script 
 
+# Boot Control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl.recovery \
+    android.hardware.boot@1.0-impl \
+
+ # Update engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+    checkpoint_gc
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+Fastbootd
+PRODUCT_PACKAGES += \
+	fastbootd \
+	android.hardware.fastboot@1.0-impl-mock.so
+
+# Gatekeeper
+PRODUCT_PACKAGES += \
+	android.hardware.gatekeeper@1.0-impl.so
+
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.0-impl-2.1.so \
+    android.hardware.health@2.0-impl-default.so
+
 # Additional binaries & libraries needed for recovery
 TARGET_RECOVERY_DEVICE_MODULES += \
     libkeymaster41 \
@@ -34,12 +63,3 @@ RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 
- # Update engine
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier \
-    checkpoint_gc
-
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
