18add69039b35698dc8667e98701833883c08bc0
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-11-30 00:13:29 +0800

    Update twrp_ruby.mk

--- twrp_ruby.mk
+++ twrp_ruby.mk
@@ -9,14 +9,14 @@
   
  # Inherit from those products. Most specific first. 
   $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk) 
-  $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk) 
+  $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk) 
   
- # Inherit some common TWRP stuff 
-  $(call inherit-product, vendor/twrp/config/common.mk) 
-  
- # Inherit from ruby device 
-  $(call inherit-product, device/xiaomi/ruby/device.mk) 
-  
+ # Inherit from ruby device
+ $(call inherit-product, device/xiaomi/ruby/device.mk)
+
+ # Inherit from our custom product configuration
+ $(call inherit-product, vendor/twrp/config/common.mk)
+
  # Device identifier. This must come after all inclusions  
   PRODUCT_DEVICE := ruby 
   PRODUCT_NAME := twrp_ruby 
