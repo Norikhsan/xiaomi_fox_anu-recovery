223fdfe05b7788fbb34c2c60ec9d9e00b5f0db2c
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-11-30 17:47:03 +0800

    Update twrp_ruby.mk

--- twrp_ruby.mk
+++ twrp_ruby.mk
@@ -1,25 +1,23 @@
- # 
- # Copyright (C) 2023 The Android Open Source Project 
- # 
- # SPDX-License-Identifier: Apache-2.0 
- # 
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
- # Release name 
- PRODUCT_RELEASE_NAME := ruby
-  
- # Inherit from those products. Most specific first. 
-  $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk) 
-  $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk) 
-  
- # Inherit from ruby device
- $(call inherit-product, device/xiaomi/ruby/device.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
- # Inherit from our custom product configuration
- $(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
- # Device identifier. This must come after all inclusions  
-  PRODUCT_DEVICE := ruby 
-  PRODUCT_NAME := twrp_ruby 
-  PRODUCT_BRAND := Redmi 
-  PRODUCT_MODEL := 22101316G 
-  PRODUCT_MANUFACTURER := xiaomi  
+# Inherit from viva device
+$(call inherit-product, device/xiaomi/viva/device.mk)
+
+PRODUCT_DEVICE := ruby
+PRODUCT_NAME := twrp_ruby
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := ruby
+PRODUCT_MANUFACTURER := xiaomi
+PRODUCT_RELEASE_NAME := Redmi Note 12 Pro (5G)
