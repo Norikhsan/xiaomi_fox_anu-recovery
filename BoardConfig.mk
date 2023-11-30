55a64d9680ef95fc1a3e3f64131e1db7d8c743aa
Norikhsan <137140023+Norikhsan@users.noreply.github.com>
2023-11-30 18:55:13 +0800

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,12 +1,20 @@
- # 
- # Copyright (C) 2023 The Android Open Source Project 
- # Copyright (C) 2023 SebaUbuntu's TWRP device tree generator 
- # 
- # SPDX-License-Identifier: Apache-2.0 
- #  
-  
-DEVICE_PATH := device/xiaomi/ruby 
-PREBUILT_PATH := $(DEVICE_PATH)/prebuilt 
+ #
+# Copyright (C) 2021 The TWRP Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PATH := device/xiaomi/ruby
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
@@ -21,7 +29,7 @@ AB_OTA_PARTITIONS += \
     vbmeta_vendor \
     vbmeta_system
 
-BOARD_USES_RECOVERY_AS_BOOT := true 
+BOARD_USES_RECOVERY_AS_BOOT := true
 
 # Architecture 
 TARGET_ARCH := arm64 
@@ -51,7 +59,7 @@ TARGET_OTA_ASSERT_DEVICE := ruby
 # Kernel
 BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x40078000
-BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 buildvariant=user
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_RAMDISK_OFFSET := 0x11088000
 BOARD_KERNEL_TAGS_OFFSET := 0x07c08000
@@ -62,14 +70,14 @@ BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 BOARD_KERNEL_IMAGE_NAME := Image
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-TARGET_KERNEL_CONFIG    := ruby_defconfig 
-TARGET_KERNEL_SOURCE   := kernel/xiaomi/ruby
+
+# AVB
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
 # Partitions
-BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 134217728
 BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
 BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
@@ -80,18 +88,12 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_COPY_OUT_PRODUCT := product
-BOARD_SUPER_PARTITION_SIZE := 9126805504 # TODO: Fix hardcoded value
+
+# Dynamic Partitions
+BOARD_SUPER_PARTITION_SIZE := 9126805504
 BOARD_SUPER_PARTITION_GROUPS := xiaomi_dynamic_partitions
 BOARD_XIAOMI_DYNAMIC_PARTITIONS_PARTITION_LIST := system vendor product
-BOARD_XIAOMI_DYNAMIC_PARTITIONS_SIZE := 9122611200 # TODO: Fix hardcoded value
-
-# Verified Boot 
-BOARD_AVB_ENABLE := true 
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
-
-# Power 
-ENABLE_CPUSETS := true 
-ENABLE_SCHEDBOOST := true
+BOARD_XIAOMI_DYNAMIC_PARTITIONS_SIZE := 9122611200
 
 # System as Root
 BOARD_SUPPRESS_SECURE_ERASE := true
@@ -118,7 +120,7 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 
 # Haptic
-TW_SUPPORT_INPUT_AIDL_HAPTICS := true
+TW_SUPPORT_INPUT_AIDL_HAPTICS :=true
 TW_SUPPORT_INPUT_AIDL_HAPTICS_FQNAME := "IVibrator/vibratorfeature"
 
 # Display
